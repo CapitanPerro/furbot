51b9c1c3c1072e242c2a2acd4850f51790e35b4d
FireGamingYT <jsabusiness06@gmail.com>
2020-04-16 14:15:43 +0000

    - code prints some information to console - bot now has ping/latency command - bot now has invite link command

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import discord
-
 from discord.ext import commands
 bot = commands.Bot(command_prefix='=', description='a simple test bot')
 
@@ -8,11 +6,16 @@ bot = commands.Bot(command_prefix='=', description='a simple test bot')
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
-
+    print('discriminator: #8860')
+    print('user ID: 620990340630970425')
+    print(f"Toothless is currently in {len(bot.guilds)} guilds")
 
 @bot.command(name="ping", aliases=["latency"], brief="Shows latency from bot!")
 async def greet_back_command(ctx):
     await ctx.send(f"bot ping: **{'{:.2f}'.format(bot.latency)}**ms")
 
+@bot.command(name="invite", aliases=["inv"], brief="Shows the bot's oauth link")
+async def greet_back_command(ctx):
+    await ctx.send(f"invite link: __**https://discordapp.com/api/oauth2/authorize?client_id=620990340630970425&permissions=8&scope=bot**__")
 
 bot.run('NjIwOTkwMzQwNjMwOTcwNDI1.XphJPw.eE4vpvudxp5M2He-ki-QSURq6xE')
